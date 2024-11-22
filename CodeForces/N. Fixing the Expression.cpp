e71fdbba4c72b1b80630556f5321dc71d095ebd7
iArgho <argho.roy488@gmail.com>
2024-11-22 11:49:12 +0600

    🥲

--- CodeForces/N. Fixing the Expression.cpp
+++ CodeForces/N. Fixing the Expression.cpp
@@ -1,12 +1,22 @@
 #include<iostream>
 using namespace std;
-int main(){
-    int n,a,b;
-    cin>>n;
-    for(int i=0;i<n;i++){
-        cin>>a>>b;
-        if(a==b){
-            cout<<a<<'='<<b<<endl;
-        };
+
+int main() {
+    int n, a, b;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a >> b;
+        if (a == b) {
+            cout << a << "=" << b << endl;
+        } else if (a > b) {
+            cout << a << ">" << b << endl;
+        } else if(a < b){
+            cout << a << "<" << b << endl;
+        } else if(a>!b){
+            cout << a << "<" << 0 << endl;}
+        else{
+            cout << b << ">" << 0 << endl;
+        }
     }
+    return 0;
 }
---
+++ cpp/circle area.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+    double r,pi;
+    pi=acos(-1);
+    cin>>r;
+    cout<<r*r*pi<<endl;
+}
