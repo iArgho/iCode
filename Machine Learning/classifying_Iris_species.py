f98b13f4a2f6ab9cc572ada0d791cfe3c2e61ad5
iArgho <argho.roy488@gmail.com>
2025-01-10 00:19:17 +0600

    scatter matrix

--- Machine Learning/classifying_Iris_species.py
+++ Machine Learning/classifying_Iris_species.py
@@ -1,11 +1,46 @@
 from sklearn.datasets import load_iris
-iris_dataset=load_iris()
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import matplotlib.pyplot as plt
+from pandas.plotting import scatter_matrix
+import mglearn
 
+iris_dataset = load_iris()
+
+# Explore the dataset
 print("Keys of iris_dataset: \n{}".format(iris_dataset.keys()))
-print(iris_dataset['DESCR'][:193]+"\n")
+print(iris_dataset['DESCR'][:193] + "\n")
 
-print("Target names:{}".format(iris_dataset['target_names']))
-print("Feature names:{}".format(iris_dataset['feature_names']))
+print("Target names: {}".format(iris_dataset['target_names']))
+print("Feature names: {}".format(iris_dataset['feature_names']))
 print("Type of data: {}".format(type(iris_dataset['data'])))
-print("Shape of data:{}".format(iris_dataset['data'].shape))
-print("First 5 columns of data:{}".format(iris_dataset['data'][:5]))
+print("Shape of data: {}".format(iris_dataset['data'].shape))
+print("First 5 columns of data:\n{}".format(iris_dataset['data'][:5]))
+print("Target:\n{}".format(iris_dataset['target']))
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(
+    iris_dataset['data'], iris_dataset['target'], random_state=0
+)
+
+print("X_train shape: {}".format(X_train.shape))
+print("y_train shape: {}".format(y_train.shape))
+print("X_test shape: {}".format(X_test.shape))
+print("y_test shape: {}".format(y_test.shape))
+
+# Create a DataFrame from the training data
+iris_dataframe = pd.DataFrame(X_train, columns=iris_dataset['feature_names'])
+
+# Create a scatter matrix
+grr = scatter_matrix(
+    iris_dataframe, 
+    c=y_train, 
+    figsize=(15, 15), 
+    marker='o', 
+    hist_kwds={'bins': 20}, 
+    s=60, 
+    alpha=.8, 
+    cmap=mglearn.cm3
+)
+
+plt.show()
